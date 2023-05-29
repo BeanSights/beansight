ea47025f7959a297b8d42ebfd0a84c78f4310b4f
udonggi <dongki1882@naver.com>
2023-05-30 00:10:35 +0900

    feat: 카페 정보 search 통해 찾기

--- src/main/java/com/ll/beansight/boundedContext/cafeInfo/controller/CafeInfoController.java
+++ src/main/java/com/ll/beansight/boundedContext/cafeInfo/controller/CafeInfoController.java
@@ -1,13 +1,16 @@
 package com.ll.beansight.boundedContext.cafeInfo.controller;
 
+import com.ll.beansight.base.api.dto.DocumentDTO;
 import com.ll.beansight.boundedContext.cafeInfo.entity.CafeInfo;
 import com.ll.beansight.boundedContext.cafeInfo.service.CafeInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -18,10 +21,14 @@ public class CafeInfoController {
     private final CafeInfoService cafeInfoService;
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/{cafeId}")
-    public String showInfo(@PathVariable Long cafeId) {
-        Optional<CafeInfo> cafeInfo = cafeInfoService.findBycafeId(cafeId);
-        //TODO: cafeId에 해당하는 정보 가져오기
+    @GetMapping("/")
+    public String showInfo(Model model, @RequestParam(defaultValue = "126.97890911337976") double x,
+                           @RequestParam(defaultValue = "37.571150829509854") double y){
+        DocumentDTO cafeInfoResponse = cafeInfoService.search(x, y);
+        Optional<CafeInfo> cafeInfo = cafeInfoService.findBycafeId(cafeInfoResponse.getId());
+        model.addAttribute("cafeInfo", cafeInfoResponse);
+
+
 
 
         return "usr/cafeInfo/showInfo";
--- src/main/java/com/ll/beansight/boundedContext/cafeInfo/service/CafeInfoService.java
+++ src/main/java/com/ll/beansight/boundedContext/cafeInfo/service/CafeInfoService.java
@@ -1,11 +1,14 @@
 package com.ll.beansight.boundedContext.cafeInfo.service;
 
+import com.ll.beansight.base.api.dto.DocumentDTO;
+import com.ll.beansight.base.api.service.KakaoSearchService;
 import com.ll.beansight.boundedContext.cafeInfo.entity.CafeInfo;
 import com.ll.beansight.boundedContext.cafeInfo.repository.CafeInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,9 +16,18 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class CafeInfoService {
     private final CafeInfoRepository cafeInfoRepository;
+    private final KakaoSearchService kakaoSearchService;
 
     public Optional<CafeInfo> findBycafeId(Long cafeId) {
 
         return cafeInfoRepository.findByCafeId(cafeId);
+    }
+
+
+    public DocumentDTO search(double x, double y) {
+        List<DocumentDTO> responses = kakaoSearchService.requestCategorySearch(x, y).getDocumentDTOList();
+        DocumentDTO response = responses.get(0);
+//        saveCafe(responses);
+        return response;
     }
 }
