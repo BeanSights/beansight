f231cab53a8dc9803fbedc83c40e65dd2dfc24de
Aidennnn97 <aidennnn97@gmail.com>
2023-05-29 15:46:35 +0900

    feat: 태그, 리뷰, 찜목록 엔티티 생성

---
+++ src/main/java/com/ll/beansight/boundedContext/review/entity/Review.java
@@ -0,0 +1,25 @@
+package com.ll.beansight.boundedContext.review.entity;
+
+import com.ll.beansight.base.baseEntity.BaseEntity;
+import com.ll.beansight.boundedContext.search.entity.Cafe;
+import com.ll.beansight.boundedContext.tag.entity.Tag;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review extends BaseEntity {
+    private Long memberId;
+    @Column(length = 100)
+    private String content;
+    @ManyToOne
+    private Cafe cafe;
+    @OneToMany(mappedBy = "review", cascade = CascadeType.REMOVE)
+    private List<Tag> tagList;
+}
--- src/main/java/com/ll/beansight/boundedContext/search/entity/Cafe.java
+++ src/main/java/com/ll/beansight/boundedContext/search/entity/Cafe.java
@@ -1,11 +1,16 @@
 package com.ll.beansight.boundedContext.search.entity;
 
 import com.ll.beansight.base.baseEntity.BaseEntity;
+import com.ll.beansight.boundedContext.review.entity.Review;
+import com.ll.beansight.boundedContext.tag.entity.Tag;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @Entity
 @SuperBuilder
@@ -18,5 +23,9 @@ public class Cafe extends BaseEntity {
     private double cafeX;
     private double cafeY;
     private String cafeName;
+    @OneToMany
+    private List<Review> reviewList;
+    @OneToMany
+    private List<Tag> tagList;
 
 }
