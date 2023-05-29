4cadb1ff1b8f37de0edfa86c4c612635a0eb494b
Jeongmin Lee <dlwogns9807@naver.com>
2023-05-29 17:12:55 +0900

    fix: 키워드로 카페 불러오는거 restcontroller로 변경

--- src/main/java/com/ll/beansight/boundedContext/search/controller/SearchController.java
+++ src/main/java/com/ll/beansight/boundedContext/search/controller/SearchController.java
@@ -2,19 +2,17 @@ package com.ll.beansight.boundedContext.search.controller;
 
 import com.ll.beansight.base.api.dto.DocumentDTO;
 import com.ll.beansight.base.rq.Rq;
+import com.ll.beansight.base.rsData.RsData;
 import com.ll.beansight.boundedContext.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/search")
 @RequiredArgsConstructor
 public class SearchController {
@@ -33,19 +31,18 @@ public class SearchController {
 
     // 키워드로 카페 불러오는
     @GetMapping("keyword/{keyword}")
-    public String keywordCafe(@PathVariable String keyword, Model model,
+    public RsData<List<DocumentDTO>> keywordCafe(@PathVariable String keyword, Model model,
                               @RequestParam(defaultValue = "126.97890911337976") double x, @RequestParam(defaultValue = "37.571150829509854") double y){
         if(keyword.isEmpty()){
-            return rq.historyBack("키워드를 입력해주세요.");
+            return RsData.of("F-1", "키워드를 입력해주세요.");
         }
         List<DocumentDTO> keywordResponse = searchService.keywordSearch(keyword, x, y);
         // 키워드로 검색했는데 결과가 없는 경우.
         if(keywordResponse.size() == 0){
-            return rq.historyBack("검색된 결과가 없습니다.");
+            return RsData.of("F-1", "검색된 결과가 없습니다.");
         }
-        model.addAttribute("keywords", keywordResponse);
 
-        return "usr/home/map";
+        return RsData.of("S-1", "키워드로 장소 검색 성공", keywordResponse);
     }
 
 
