aceb5e347e1b29edd0fb33466bd771ceb051d29a
Jeongmin Lee <dlwogns9807@naver.com>
2023-05-30 22:57:13 +0900

    fix: 키워드 검색 컨트롤러 response 수정

--- src/main/java/com/ll/beansight/boundedContext/search/controller/SearchController.java
+++ src/main/java/com/ll/beansight/boundedContext/search/controller/SearchController.java
@@ -8,8 +8,10 @@ import com.ll.beansight.boundedContext.cafeInfo.repository.CafeInfoRepository;
 import com.ll.beansight.boundedContext.search.entity.Cafe;
 import com.ll.beansight.boundedContext.search.repository.CafeRepository;
 import com.ll.beansight.boundedContext.search.service.SearchService;
+import com.ll.beansight.standard.util.Ut;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -37,18 +39,19 @@ public class SearchController {
 
     // 키워드로 카페 불러오는
     @GetMapping("keyword/{keyword}")
-    public RsData<List<DocumentDTO>> keywordCafe(@PathVariable String keyword, Model model,
-                              @RequestParam(defaultValue = "126.97890911337976") double x, @RequestParam(defaultValue = "37.571150829509854") double y){
+    public ResponseEntity<RsData> keywordCafe(@PathVariable String keyword, Model model,
+                                              @RequestParam(defaultValue = "126.97890911337976") double x, @RequestParam(defaultValue = "37.571150829509854") double y){
         if(keyword.isEmpty()){
-            return RsData.of("F-1", "키워드를 입력해주세요.");
+            return Ut.spring.responseEntityOf(RsData.of("F-1", "키워드를 입력해주세요."));
         }
         List<DocumentDTO> keywordResponse = searchService.keywordSearch(keyword, x, y);
         // 키워드로 검색했는데 결과가 없는 경우.
         if(keywordResponse.size() == 0){
-            return RsData.of("F-1", "검색된 결과가 없습니다.");
+            return Ut.spring.responseEntityOf(RsData.of("F-1", "검색된 결과가 없습니다."));
+
         }
 
-        return RsData.of("S-1", "키워드로 장소 검색 성공", keywordResponse);
+        return Ut.spring.responseEntityOf(RsData.of("S-1", "키워드로 장소 검색 성공", keywordResponse));
     }
 
     // 필터링 기능
