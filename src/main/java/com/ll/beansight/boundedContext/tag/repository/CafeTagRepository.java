97d3dc40e959a206c0ef4322196607fe078d89d0
udonggi <dongki1882@naver.com>
2023-06-07 13:39:05 +0900

    fix: Tag tag

--- src/main/java/com/ll/beansight/boundedContext/tag/repository/CafeTagRepository.java
+++ src/main/java/com/ll/beansight/boundedContext/tag/repository/CafeTagRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CafeTagRepository extends JpaRepository<CafeTag, Long> {
-    List<CafeTag> findAllByTagId_TagId(Long tagId);
-    boolean existsByTagId_TagIdAndCafeInfo(Long tagId, CafeInfo cafeInfo);
+    List<CafeTag> findAllByTag_TagId(Long tagId);
+    boolean existsByTag_TagIdAndCafeInfo(Long tagId, CafeInfo cafeInfo);
 
 }
