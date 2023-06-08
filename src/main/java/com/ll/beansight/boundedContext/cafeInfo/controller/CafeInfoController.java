430eaa6b870cb3545b75713591d1f157a8e4d05f
udonggi <dongki1882@naver.com>
2023-06-08 15:42:31 +0900

    feat: 찜 구현 뼈대 잡기

--- src/main/java/com/ll/beansight/boundedContext/cafeInfo/controller/CafeInfoController.java
+++ src/main/java/com/ll/beansight/boundedContext/cafeInfo/controller/CafeInfoController.java
@@ -1,9 +1,14 @@
 package com.ll.beansight.boundedContext.cafeInfo.controller;
 
 import com.ll.beansight.base.api.dto.DocumentDTO;
+import com.ll.beansight.base.rq.Rq;
 import com.ll.beansight.boundedContext.cafeInfo.entity.CafeInfo;
 import com.ll.beansight.boundedContext.cafeInfo.service.CafeInfoService;
 import com.ll.beansight.boundedContext.tag.entity.CafeTag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -19,6 +24,7 @@ import java.util.Optional;
 @RequestMapping("/cafeInfo")
 public class CafeInfoController {
     private final CafeInfoService cafeInfoService;
+    private final Rq rq;
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("")
@@ -29,5 +35,19 @@ public class CafeInfoController {
         model.addAttribute("cafeInfo", cafeInfoResponse);
 
         return "usr/cafeInfo/showInfo";
+    }
+
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("")
+    public String showInfo(@RequestParam(defaultValue = "126.97890911337976") double x,
+                           @RequestParam(defaultValue = "37.571150829509854") double y, @RequestParam("cafeList") List<String> cafeList, HttpServletRequest request){
+        CafeInfo cafeInfoResponse = cafeInfoService.search(x, y);
+        String a = cafeList.get(1);
+
+
+
+        String referer = request.getHeader("Referer");
+        return rq.redirectWithMsg("/cafeInfo?x="+x+"&y="+y, "good");
     }
 }
